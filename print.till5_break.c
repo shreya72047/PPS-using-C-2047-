2c76e3bb81303033283d20449c14b6d7d0080045
shreya72047 <shreyasarkar2100@gmail.com>
2025-05-23 01:32:36 +0530

    Add files via upload

---
+++ print.till5_break.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int i;
+  for(i=0; i<11; i++)
+    {
+      if(i==5)
+      {
+       break;
+      } 
+        printf("%d\n",i);
+    }    
+     printf("END");
+    
+ return 0;
+}   
